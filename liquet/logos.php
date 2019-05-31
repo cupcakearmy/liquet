206dfa802759c3cd8e548ac7c483c5c42c5f5b4e
cupcakearmy <nicco.borgioli@gmail.com>
2019-06-01 00:17:34 +0200

    beautify code

--- liquet/logos.php
+++ liquet/logos.php
@@ -8,12 +8,11 @@ function render_links( $expanded = false ) {
     <div id="logos">
 	<?php if ( $expanded ) { ?>
         <div id="title" class="alt-font">
-        discuss & share
-        <br/>
-        <img src="<?= get_template_directory_uri() . '/vendor/icons/down.svg' ?>"
-             alt="arrow down"/>
+            discuss & share
+            <br/>
+            <img src="<?= get_template_directory_uri() . '/vendor/icons/down.svg' ?>" alt="arrow down"/>
         </div>
-    <?php } ?>
+	<?php } ?>
     <div class="flex container horizontal middle center" id="list">
 		<?php
 		foreach ( $links as $link ) {
