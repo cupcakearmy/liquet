de8b59f07c3bc7e72d1189f07583ecbdfb60f855
cupcakearmy <nicco.borgioli@gmail.com>
2019-06-15 09:49:22 +0200

    safari fix for flexbox

--- liquet/singular.php
+++ liquet/singular.php
@@ -22,64 +22,66 @@ $dates = getDates();
                     <div class="alt-font">previous</div>
                 </a>
 
-                <div class="flex item grow container vertical middle" id="main">
-
-                    <!-- Title -->
+                <div class="flex item grow" id="main">
                     <div class="flex container vertical middle">
-                        <div class="box" id="header">
-                            <h1 class="alt-font"><?= the_title() ?></h1>
+
+                        <!-- Title -->
+                        <div class="flex container vertical middle">
+                            <div class="box" id="header">
+                                <h1 class="alt-font"><?= the_title() ?></h1>
+                            </div>
                         </div>
-                    </div>
 
-                    <!-- Content Body -->
-                    <div class="flex container vertical middle" id="content">
+                        <!-- Content Body -->
+                        <div class="flex container vertical middle" id="content">
 
-                        <!-- Reading time and date -->
+                            <!-- Reading time and date -->
+							<?php if ( is_singular( 'post' ) ) : ?>
+                                <div class="flex container horizontal">
+                                    <div>
+                                        <b><?= $dates['created'] ?></b>
+										<?php if ( $dates['different'] ) { ?>
+                                            <br/>
+                                            <small>updated on <?= $dates['modified'] ?></small>
+										<?php } ?>
+                                    </div>
+                                    <div class="flex item grow"></div>
+                                    <div class="text-align-right">
+                                        <b>~<?= do_shortcode( '[rt_reading_time]' ); ?> min</b>
+										<?= getCurrentPageViews() ?>
+                                    </div>
+                                </div>
+                                <br><br>
+							<?php endif; ?>
+
+                            <!-- Content -->
+							<?php the_content(); ?>
+                        </div>
+
+                        <!-- Tags -->
 						<?php if ( is_singular( 'post' ) ) : ?>
-                            <div class="flex container horizontal">
-                                <div>
-                                    <b><?= $dates['created'] ?></b>
-									<?php if ( $dates['different'] ) { ?>
-                                        <br/>
-                                        <small>updated on <?= $dates['modified'] ?></small>
-									<?php } ?>
-                                </div>
-                                <div class="flex item grow"></div>
-                                <div class="text-align-right">
-                                    <b>~<?= do_shortcode( '[rt_reading_time]' ); ?> min</b>
-									<?= getCurrentPageViews() ?>
-                                </div>
+                            <div class="auto-width text-align-center">
+                                <br>
+								<?php render_current_tags(); ?>
+                                <br>
                             </div>
-                            <br><br>
 						<?php endif; ?>
 
-                        <!-- Content -->
-						<?php the_content(); ?>
-                    </div>
-
-                    <!-- Tags -->
-					<?php if ( is_singular( 'post' ) ) : ?>
-                        <div class="auto-width text-align-center">
-                            <br>
-							<?php render_current_tags(); ?>
-                            <br>
+                        <!-- Links -->
+                        <div class="flex container vertical middle auto-width">
+							<?php render_links( true ); ?>
                         </div>
-					<?php endif; ?>
 
-                    <!-- Links -->
-                    <div class="flex container vertical middle auto-width">
-						<?php render_links( true ); ?>
-                    </div>
-
-                    <!-- Bottom Widgets -->
-					<?php if ( is_singular( 'post' ) ) : ?>
-						<?php if ( is_active_sidebar( 'below_posts' ) ) : ?>
-                            <br><br><br><br><br><br>
-                            <div class="auto-width widget-area text-align-center">
-								<?php dynamic_sidebar( 'below_posts' ); ?>
-                            </div>
+                        <!-- Bottom Widgets -->
+						<?php if ( is_singular( 'post' ) ) : ?>
+							<?php if ( is_active_sidebar( 'below_posts' ) ) : ?>
+                                <br><br><br><br><br><br>
+                                <div class="auto-width widget-area text-align-center">
+									<?php dynamic_sidebar( 'below_posts' ); ?>
+                                </div>
+							<?php endif; ?>
 						<?php endif; ?>
-					<?php endif; ?>
+                    </div>
                 </div>
 
                 <a class="flex item shrink links" href="<?= getNextURL( true ) ?>">
