8c70403a1c3c2366a79f8900f8d96018c9475a5e
cupcakearmy <nicco.borgioli@gmail.com>
2019-07-03 10:32:05 +0200

    support for thumbnails

--- liquet/functions.php
+++ liquet/functions.php
@@ -4,6 +4,7 @@ include_once( 'logos.php' );
 
 add_theme_support( 'align-wide' );
 add_theme_support( 'title-tag' );
+add_theme_support( 'post-thumbnails' );
 
 add_action( 'wp_enqueue_scripts', function () {
 	wp_enqueue_style( 'flex', get_template_directory_uri() . '/css/flex.css' );
--- liquet/post-preview.php
+++ liquet/post-preview.php
@@ -1,26 +1,30 @@
 <?php
 
-include_once('loop-utils.php');
+include_once( 'loop-utils.php' );
 
 $dates = getDates();
 ?>
 
 <a href="<?= get_permalink() ?>" class="item">
     <div class="flex container vertical">
-
+		<?php if ( has_post_thumbnail() ) : ?>
+            <div class="thumbnail">
+				<?= get_the_post_thumbnail(); ?>
+            </div>
+		<?php endif; ?>
         <h2 class="alt-font title"><?php the_title(); ?></h2>
         <div class="flex container horizontal">
             <div>
-                <?= $dates['created'] ?>
+				<?= $dates['created'] ?>
             </div>
             <div class="flex item grow"></div>
             <div>
-                ~<?= do_shortcode('[rt_reading_time]'); ?> min
+                ~<?= do_shortcode( '[rt_reading_time]' ); ?> min
             </div>
         </div>
-        <?php if(has_excerpt()){
-            the_excerpt();
-        } ?>
+		<?php if ( has_excerpt() ) {
+			the_excerpt();
+		} ?>
     </div>
 </a>
 <hr/>
